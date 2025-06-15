d3c65ffd56cf395a4df61a5d6bf6c241120c1872
Siegerus <ivanoffsiegerus@gmail.com>
2025-06-15 21:40:07 +0300

    bfr footer nav mb fix

---
+++ www/toapinfo.ru/mg-pages/new_page.php
@@ -0,0 +1,13 @@
+<?php
+/* MG::enableTemplate(); // Подключает каркас шаблона */
+MG::titlePage('Пример страницы'); // задает title для страницы
+
+/** Любой код на PHP*/
+print_r('Вывод из PHP');
+?>
+
+<h1>
+<!-- Любая верстка -->
+Пример страницы
+<div class="">Lorem ipsum dolor sit amet.</div>
+</h1>
